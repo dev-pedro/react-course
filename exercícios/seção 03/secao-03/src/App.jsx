c82233668d8b446058f35a2b6880acbd3a33d681
dev-pedro <dev.pedro.rjas@gmail.com>
2023-09-07 18:34:13 -0300

    Desafio sobre criação de apps em React

---
+++ exercícios/seção 03/secao-03/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>Avançando no React!</h1>
+        <h2>Seção - 03</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
